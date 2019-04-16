4c1dd27e0be4ecfebd8ca677a3a1c8df926fc72a
AngusParsonson <amfparsonson@gmail.com>
2019-04-16 19:14:45 +0100

    second commity boi

--- wine_classifier.py
+++ wine_classifier.py
@@ -21,18 +21,65 @@ CLASS_3_C = r'#ffc34d'
 
 MODES = ['feature_sel', 'knn', 'alt', 'knn_3d', 'knn_pca']    
 
+# Feature Selection ------------------------------------------------------------------------------------------------------
+
+def reduce_data(train_set, test_set, selected_features):
+    train_set_red = train_set[:, selected_features]
+    test_set_red  = test_set[:, selected_features]
+    
+    return train_set_red, test_set_red
 
 def feature_selection(train_set, train_labels, **kwargs):
-    # write your code here and make sure you return the features at the end of 
-    # the function
-    return []
 
+    number_of_features = 2
+    train_set_red, test_set_red = reduce_data(train_set, test_set, [10,12])
+    
+    n_features = train_set.shape[1]
+    fig, ax = plt.subplots(number_of_features, number_of_features)
+    plt.subplots_adjust(left=0.01, right=0.99, top=0.99, bottom=0.01, wspace=0.2, hspace=0.4)
+
+    class_1_colour = r'#3366ff'
+    class_2_colour = r'#cc3300'
+    class_3_colour = r'#ffc34d'
+
+    class_colours = [class_1_colour, class_2_colour, class_3_colour]
+
+    # write your code here
+    colours = np.zeros_like(train_labels, dtype=np.object)
+    colours[train_labels == 1] = class_1_colour
+    colours[train_labels == 2] = class_2_colour
+    colours[train_labels == 3] = class_3_colour
+
+    for row in range(number_of_features):
+        for col in range(number_of_features):
+            ax[row][col].scatter(train_set_red[:, row], train_set_red[:, col], c=colours)
+            ax[row][col].set_title('Features {} vs {}'.format(row+1, col+1))
+
+    plt.show()
+    
+# Knn -------------------------------------------------------------------------------------------------------------------
+    
+def calculate_centroids(train_set, train_labels):
+    classes = np.unique(train_labels)
+    centroids = np.array([np.mean(train_set[train_labels == c, :], axis=0) for c in classes])
+    
+    return centroids, classes
+
+def nearest_centroid(centroids, test_set):
+    dist = lambda x,y : np.sqrt(np.sum((x-y)**2))
+    centroid_dist = lambda x : [dist(x, centroid) for centroid in centroids]
+    predicted = np.argmin([centroid_dist(p) for p in test_set], axis = 1).astype(np.int) + 1
+    
+    return predicted
 
 def knn(train_set, train_labels, test_set, k, **kwargs):
-    # write your code here and make sure you return the predictions at the end of 
-    # the function
-    return []
+    centroids, classes = calculate_centroids(train_set_red, train_labels)
+    
+    predicted = nearest_centroid(centroids, test_set_red)
+    np.savetxt('results.csv', predicted, delimiter=',', fmt='%d')
 
+    for i, pred in enumerate(predicted):
+        print('{:02d} gt class: {}\tpredicted class: {}'.format(i+1, pred, test_labels[i]))
 
 def alternative_classifier(train_set, train_labels, test_set, **kwargs):
     # write your code here and make sure you return the predictions at the end of 
